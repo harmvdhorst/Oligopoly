e53f8f67e50a169a9b1472ded252b740ea07557d
laorpaksa900 <jaydee.laorpaksa@hotmail.com>
2025-03-26 10:54:41 +0100

    yup

---
+++ src/main/java/nl/harmjaydee/oligopoly/functions/Card.java
@@ -0,0 +1,15 @@
+package nl.harmjaydee.oligopoly.functions;
+
+
+public class Card {
+    private String description;
+
+    public Card(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    }
+
--- src/main/java/nl/harmjaydee/oligopoly/functions/Cards.java
+++ src/main/java/nl/harmjaydee/oligopoly/functions/Cards.java
@@ -1,11 +1,21 @@
 package nl.harmjaydee.oligopoly.functions;
 
+
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Cards {
-    private List cards;
-    public void chanceCard() {
+    private List<Card> cardList;
+    public Cards(String type) {
+        this.cardList = new ArrayList<>();
+    }
+    private void CreateCards() {
+    // voeg kaarten toe
+    }
 
+    public List<Card> getCards() {
+        return new ArrayList<>(cardList);
     }
     public void draw(Graphics g) {
 
